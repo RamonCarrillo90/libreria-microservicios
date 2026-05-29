638bdb04ec394cee93091ecbcb738b6b8cb770f5
RamonCarrillo90 <carrilloriverar70@gmail.com>
2026-05-29 00:13:24 -0700

    cambios en el middleware 2

--- ordenes-service/index.js
+++ ordenes-service/index.js
@@ -8,6 +8,13 @@ app.use(express.json());
 const CATALOGO_URL = process.env.CATALOGO_URL || 'https://libreria-microservicios.vercel.app';
 const ordenes = [];
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    next();
+});
+
 app.post('/api/ordenes', async (req, res) => {
     const { libroId, cantidad, cliente } = req.body;
 
